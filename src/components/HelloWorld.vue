69a89aca4522d45e2483176b85b31af608c68d58
kziete <sebastian@elmundoalreves.cl>
2018-05-28 22:33:17 -0400

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-
-    <componente-simple v-for="item in items" 
-      :key="item"
-      :mensaje="item"
-      @notificar="notificar_papa">
-    </componente-simple>
-
-  </div>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
-
-<script>
-import ComponenteSimple from '@/components/ComponenteSimple'
-
-export default {
-  components: {
-    ComponenteSimple
-  },
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      items: ["Primero", "Segundo", "Tercero"]
-    }
-  },
-  methods:{
-    notificar_papa(hijo){
-      this.msg = `${hijo} clickeado`;
-    }
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
