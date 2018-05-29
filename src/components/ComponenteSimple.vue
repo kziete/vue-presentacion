d048fed380e6b58a321eff9d2beabaabf41aa1e1
kziete <sebastian@elmundoalreves.cl>
2018-05-28 22:21:41 -0400

    componente simple

---
+++ src/components/ComponenteSimple.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{mensaje}}</h2>
+        <button @click="notificar">Notificar</button>
+    </div>
+</template>
+<script>
+export default {
+  props: ["mensaje"],
+  data(){
+      return {}
+  },
+  methods: {
+      notificar(){
+          this.$emit("notificar",this.mensaje);
+      }
+  }
+}
+</script>
+
